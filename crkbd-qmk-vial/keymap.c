504c97c889a8ce49e4a4dd8f079dc4a17f64efba
zehael <evg29227704@gmai.com>
2024-03-13 23:20:08 +0500

    Update Caps Words mode

--- crkbd-qmk-vial/keymap.c
+++ crkbd-qmk-vial/keymap.c
@@ -80,13 +80,13 @@ bool caps_word_press_user(uint16_t keycode) {
         case KC_GRV:
         case KC_DOT:
         case KC_COMM:
-        case KC_MINS:
             add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
             return true;
 
         // Keycodes that continue Caps Word, without shifting.
         case KC_1 ... KC_0:
         case KC_BSPC:
+        case KC_MINS:
         case KC_DEL:
         case KC_UNDS:
             return true;
